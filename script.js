6e496fefff8c18e379f0e63733fafefec33978b0
EasyCode2011 <andyzhaoyucheng@gmail.com>
2025-12-08 15:39:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ document.getElementById("btnRectangles").addEventListener("click", () => {
     rec.classList.add("fade-in");
   }, 600);
 });
+const glowBtn = document.getElementById("glowBtn");
+
+// Generate random color
+function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+let nextColor = randomColor();
+
+glowBtn.addEventListener("mouseenter", () => {
+  glowBtn.style.backgroundColor = nextColor;
+
+  // prepare next random color for next hover
+  nextColor = randomColor();
+});
+
+glowBtn.addEventListener("mouseleave", () => {
+  glowBtn.style.backgroundColor = "white";
+});
