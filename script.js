81145311bf00ef68599f4b462cb782e854c8b86d
EasyCode2011 <andyzhaoyucheng@gmail.com>
2025-12-18 09:46:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -280,14 +280,9 @@ const btnSliders = document.getElementById("btnSliders");
 const sliderPage = document.getElementById("sliderPage");
 
 btnSliders.addEventListener("click", () => {
-  // hide everything else
-  home.classList.add("hidden");
-  rectPage.classList.add("hidden");
-  introScene.classList.add("hidden");
-
-  // show slider page
-  sliderPage.classList.remove("hidden");
-  sliderPage.classList.add("fade-in");
+  scene = 4;
+  showPage(sliderPage);
+  document.getElementById("menu").classList.remove("open");
 });
 
 
